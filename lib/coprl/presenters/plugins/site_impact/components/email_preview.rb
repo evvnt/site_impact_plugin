b915a58f300f62811c700f3cd71e2a6cd8b7b675
Derek Graham <derek@evvnt.com>
2024-02-15 13:26:48 -0500

    Avoid calls to attribute accessors that end in _url as they can clash with helper methods in core.

--- lib/coprl/presenters/plugins/site_impact/components/email_preview.rb
+++ lib/coprl/presenters/plugins/site_impact/components/email_preview.rb
@@ -7,29 +7,23 @@ module Coprl
       module SiteImpact
         module Components
           class EmailPreview < DSL::Components::EventBase
-            attr_reader :preview_url, :persist_url, :preview_email_url, :button_edit_text, :button_save_text,
+            attr_reader :preview_url, :persist_url, :button_edit_text, :button_save_text,
                         :button_cancel_text, :button_send_text, :button_color, :view_only
             def initialize(**attribs, &block)
               @preview_url = attribs.delete(:preview_url)
               @persist_url = attribs.delete(:persist_url)
-              @preview_email_url = attribs.delete(:preview_email_url)
               @button_edit_text = attribs.delete(:button_edit_text){ 'Edit' }
               @button_save_text = attribs.delete(:button_save_text){ 'Save' }
               @button_cancel_text = attribs.delete(:button_cancel_text){ 'Cancel' }
               @button_send_text = attribs.delete(:button_send_text){ nil }
               @button_color = attribs.delete(:button_color){ :primary }
               @view_only = attribs.delete(:view_only){ false }
-              @component_options = %i(preview_url persist_url preview_email_url)
               super(type: :email_preview, **attribs, &block)
               expand!
             end
 
             def email_preview_options
-              @component_options.map do |key|
-                if (value = send(key))
-                  [key, value]
-                end
-              end.compact.to_h
+              {preview_url: @preview_url, persist_url: @persist_url}
             end
 
           end
